1ef567b8dba38b2a5940b7dd25dfee019435fbe7
Aeden <florentius.b@gmail.com>
2018-08-05 22:34:28 +0200

    Amélioration du anidb_importer

--- tools/anidb_importer/index.js
+++ tools/anidb_importer/index.js
@@ -28,28 +28,31 @@ async function main(xmlFile, jsonFile) {
 				return false;
 			})) {
 				// X-jat is found. Parse our JSON to determine if we have that series
-				if (json.series[i].name === anime.title[xjatIndex]._) {						animeFound = true;
+				var karaokeMugenTitle = json.series[i].name.replace('ô','ou').replace('û','uu').replace(/[\ \-\:\!\.☆♪]/g,'').toUpperCase();
+				var aniDbTitle = anime.title[xjatIndex]._.replace(/[\ \-\:\!\.☆♪]/g,'').toUpperCase()
+				if (karaokeMugenTitle === aniDbTitle) {
+					animeFound = true;
 					// Series found!
 					// Parse all entries
 					for (const title of anime.title) {
-						if (title.$.type === 'syn' || title.$.type === 'short') {
+						if (title.$.type === 'syn' || title.$.type === 'short' || title.$['xml:lang'] == 'ja') {
 							// Title should be added to aliases if it doesn't exist already
 							if (json.series[i].aliases) {
-								if (!json.series[i].aliases.includes(title._)) json.series[i].aliases.push(title._);
+								var aliasesUpperCase = String(json.series[i].aliases).toUpperCase().split(",");
+								if (!aliasesUpperCase.includes(title._.toUpperCase())) json.series[i].aliases.push(title._);
 							} else {
-								json.series[i].aliases = [title._];
+								json.series[i] = { "name": json.series[i].name, aliases: [title._], "i18n": json.series[i].i18n };
 							}
-						}
-						if (title.$.type === 'main' || title.$.type === 'official') {
+						} else if (title.$.type === 'main') {
+							json.series[i].i18n['jpn'] = title._.replace('ou','ô').replace('uu','û');
+						} else if (title.$.type === 'official' && title.$['xml:lang'] !== 'x-jat') {
 							//Determine iso639-2B language
-							if (title.$['xml:lang'] !== 'x-jat') {
-								const shortLang = title.$['xml:lang'].substring(0,2);
-								if (langs.has('1',shortLang)) {
-									const lang = langs.where('1', shortLang);
-									json.series[i].i18n[lang['2B']] = title._;
-								} else {
-									console.log('Language unknown ('+title.$['xml:lang']+') for series '+anime.title[xjatIndex]._);
-								}
+							const shortLang = title.$['xml:lang'].substring(0, 2);
+							if (langs.has('1', shortLang)) {
+								const lang = langs.where('1', shortLang);
+								json.series[i].i18n[lang['2B']] = title._;
+							} else {
+								console.log('Language unknown (' + title.$['xml:lang'] + ') for series ' + anime.title[xjatIndex]._);
 							}
 						}
 					}
@@ -57,8 +60,8 @@ async function main(xmlFile, jsonFile) {
 			}
 		}
 		if (!animeFound) {
-			// Series not found in our series.json
-			console.log('WARNING : Series not found in aniDB : '+json.series[i].name);
+			// Series not found in aniDB
+			console.log('WARNING : Series not found in aniDB : '+json.series[i].name.replace('ô','ou').replace('û','uu'));
 		}
 	}
 	await writeFile('out.json',JSON.stringify(json,null,2), 'utf-8');
