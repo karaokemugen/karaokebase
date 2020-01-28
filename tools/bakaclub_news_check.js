e11a3ccf695e291bd6a4f03f3bf5694d9d7038f0
Guillaume Lebigot <axel@teri-chan.net>
2020-01-28 19:41:21 +0100

    Fix stuff [skip-ci]

--- tools/bakaclub_news_check.js
+++ tools/bakaclub_news_check.js
@@ -1,12 +1,11 @@
-const http = require('http');
 const fs = require('fs');
-
+const request = require('request');
 //Test with raw file downloaded
 //const raw = fs.readFileSync('./bakaclub.json', 'utf-8');
 //parseBakaClubKaras(JSON.parse(raw));
 
-http.get("https://kurisu.iiens.net/api_karas.php", response => {
-	parseBakaClubKaras(JSON.parse(response.body));
+request('https://kurisu.iiens.net/api_karas.php', (error, response, body) => {
+	parseBakaClubKaras(JSON.parse(body));
 });
 
 async function parseBakaClubKaras(karas) {
@@ -25,22 +24,25 @@ async function gitlabPostNewIssue(kara) {
 		['id', '2'],
 		['title', `[Import Bakaclub] ${kara.language.toUpperCase()} - ${kara.source_name} - ${kara.song_type}${kara.song_number || ''} - ${kara.song_name}`],
 		['description', `
-		Download URL : https://kurisu.iiens.net/download.php?id=${kara.id}
+Download URL : https://kurisu.iiens.net/download.php?id=${kara.id}
 
-		Auteur : ${kara.author_name}
+Auteur : ${kara.author_name}
 
-		Catégorie : ${kara.category}
-
-		`],
+Catégorie : ${kara.category}
+`],
 		['labels', 'à intégrer']
 	]);
-	const res = await http.post(`https://lab.shelter.moe/api/v4/projects/2/issues?${params.toString()}`, {
+	request.post(`https://lab.shelter.moe/api/v4/projects/2/issues?${params.toString()}`, {
 		headers: {
 			'PRIVATE-TOKEN': 'i5WnabG3fvda4oxx-FRb'
 		}
+	}, (err) => {
+		if (err) {
+			console.log(err);
+			process.exit(1);
+		}
 	});
 
-	//console.log(params.toString());
 }
 
 
