40b8eadb0d2c2bc360cf1830700b18441ed5c1da
Leon <me@leonekmi.fr>
2020-04-30 21:15:08 +0200

    [nomedia] L'espace après le type vidéo n'est plus doublé si il n'y a pas de songorder

--- tools/export_karaokesmoe.php
+++ tools/export_karaokesmoe.php
@@ -131,7 +131,7 @@ foreach ($second_pass as $serie_singer => $kara_serie_singer) {
 			//Determine song order
 			$languages = json_decode($kara['languages'], true);
 
-			$type_with_num = $type . ' ' . (!empty($kara['songorder']) ? $kara['songorder'] : '') . ' - '  . $languages[0]['name'] . ' - ' . $kara['title'];
+			$type_with_num = $type . (!empty($kara['songorder']) ? ' '.$kara['songorder'] : '') . ' - '  . $languages[0]['name'] . ' - ' . $kara['title'];
 
             $audioOnly = (0 === strpos(strrev($kara['mediafile']), strrev('.mp3'))); // ends with mp3 ? we'll consider it as audio only.
             $mimeType = $audioOnly? 'audio/mp3':'video/mp4';
