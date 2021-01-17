ebe024556e13a34390f05a572847cbf7d8dc166e
Guillaume Lebigot <axel@teri-chan.net>
2021-01-17 10:18:52 +0100

    Update bakaclub_news_check.js - fix timezones [skip-ci]

--- tools/bakaclub_news_check.js
+++ tools/bakaclub_news_check.js
@@ -5,56 +5,51 @@ const request = require('request');
 //parseBakaClubKaras(JSON.parse(raw));
 
 request('https://kurisu.iiens.net/api', (error, response, body) => {
-	if (!error) {
-		parseBakaClubKaras(JSON.parse(body));
-	} else {
-		console.log(error);
-	}
+        if (!error) {
+                //console.log(JSON.stringify(JSON.parse(body), null, 2));
+                parseBakaClubKaras(JSON.parse(body));
+        } else {
+                console.log(error);
+        }
 });
 
 async function parseBakaClubKaras(karas) {
-	const newKaras = karas.filter(k => {
-		const karaDate = new Date(k.author_year);
-		const date = new Date(process.argv[2]);
-		return karaDate >= date;
-	});
-	for (const kara of newKaras) {
-		try {
-			await gitlabPostNewIssue(kara);
-		} catch(err) {
-			// Do nothing.
-		}
-	}
+        const newKaras = karas.filter(k => k.author_year >= (process.argv[2] + ' 00:00:00'));
+        for (const kara of newKaras) {
+                try {
+                        await gitlabPostNewIssue(kara);
+                } catch(err) {
+                        // Do nothing.
+                }
+        }
 }
 
 async function gitlabPostNewIssue(kara) {
-	const params = new URLSearchParams([
-		['id', '2'],
-		['title', `[Import Bakaclub] ${kara.language.toUpperCase()} - ${kara.source_name} - ${kara.song_type}${kara.song_number || ''} - ${kara.song_name}`],
-		['description', `
+        const params = new URLSearchParams([
+                ['id', '2'],
+                ['title', `[Import Bakaclub] ${kara.language.toUpperCase()} - ${kara.source_name} - ${kara.song_type}${kara.song_number || ''} - ${kara.song_name}`],
+                ['description', `
 Download URL : https://kurisu.iiens.net/api/download/${kara.id}
 
 Auteur : ${kara.author_name}
 
 Catégorie : ${kara.category}
 `],
-		['labels', 'à intégrer']
-	]);
-	return new Promise((resolve, reject) => {
-		console.log(kara.author_year, kara.source_name, kara.song_name);
-		request.post(`https://lab.shelter.moe/api/v4/projects/2/issues?${params.toString()}`, {
-			headers: {
-				'PRIVATE-TOKEN': 'i5WnabG3fvda4oxx-FRb'
-			}
-		}, (err) => {
-			if (err) {
-				console.log(err);
-				reject();
-			} else {
-				resolve();
-			}
-		});
-	});
+                ['labels', 'à intégrer']
+        ]);
+        return new Promise((resolve, reject) => {
+                console.log(kara.author_year, kara.source_name, kara.song_name);
+                request.post(`https://lab.shelter.moe/api/v4/projects/2/issues?${params.toString()}`, {
+                        headers: {
+                                'PRIVATE-TOKEN': 'i5WnabG3fvda4oxx-FRb'
+                        }
+                }, (err) => {
+                        if (err) {
+                                console.log(err);
+                                reject();
+                        } else {
+                                resolve();
+                        }
+                });
+        });
 }
-
-
