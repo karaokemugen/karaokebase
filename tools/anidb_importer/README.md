85711910b08224eafbeeb73e7f97cb4f1c4d716f
Florent Berthelot <florentius.b@gmail.com>
2024-06-25 21:12:28 +0200

    🔥 Remove tools folder

--- tools/anidb_importer/README.md
+++
@@ -1,9 +0,0 @@
-Usage:
-
-`node index.js anidb.xml series.json >log.txt`
-
-This will produce a out.json containing the modified series.json.
-
-Log contains all series not found in AniDB XML, as well as other errors.
-
-
--- tools/delay.sh
+++
@@ -1,5 +0,0 @@
-kara=$1
-awk -F, '{if ($1 ~ /^Dialogue:/) {cmd="date -d \"1 second ago "$2"\" +%H:%M:%S.%N";cmd | getline datum; close(cmd); cmd="date -d \"0.10 second "$3"\" +%H:%M:%S.%N";cmd | getline datum2; close(cmd); $2=datum ; $3=datum2 ; gsub(/0000000$/,"",$2);gsub(/0000000$/,"",$3); gsub(/^0/,"",$2);gsub(/^0/,"",$3);$10="{\\fad(800,100)\\k90}"$10 ; print $0}}' FS=, OFS=, "$kara" >tmp.txt
-sed '/^Dialogue:/d' -i "$kara"
-cat tmp.txt >>"$kara"
-rm -f tmp.txt
